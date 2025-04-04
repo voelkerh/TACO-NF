8a97a1e981a851736a8b4f92d7d59eaa51a668e5
voelkerh <henriette.voelker@posteo.de>
2025-04-04 17:32:08 +0200

    Make taxDB abstract class

--- nextflow/conversion/Database/taxdb.py
+++ nextflow/conversion/Database/taxdb.py
@@ -1,33 +1,44 @@
-#!/usr/bin/env python3
+from abc import ABC, abstractmethod
 
-class TaxDB:
+
+class TaxDB(ABC):
+    """Abstract class defining retrieval methods for internal taxonomy database."""
+
     def __init__(self):
         pass
 
+    @abstractmethod
     def load_taxid_from_accession_number(self, accession):
         return ""
-    
+
+    @abstractmethod
     def load_names_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def load_full_ranks_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def load_parents_taxid_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def get_taxid_from_accession_number(self, accession):
         return ""
 
+    @abstractmethod
     def get_name_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def get_parent_taxid_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def get_full_rank_from_taxonomic_data(self, taxid):
         return ""
 
+    @abstractmethod
     def get_rank_code_from_full_rank(self, rank):
         return ""
-    
